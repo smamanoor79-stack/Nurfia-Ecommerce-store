db4c6869c6da0cfd7a4239b6fa179a7bf6341cdf
smamanoor <samamanoor79@gmail.com>
2026-06-26 20:22:18 +0500

    add orders.html to vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,21 +1,14 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        shop: './shop.html',
-        men: './men.html',
-        women: './women.html',
-        product: './product.html',
-        cart: './cart.html',
-        wishlist: './wishlist.html',
-        checkout: './checkout.html',
-        contact: './contact.html',
-        blog: './blog.html',
-        'blog-detail': './blog-detail.html',
-      },
-    },
-  },
-});
+input: {
+  main: './index.html',
+  shop: './shop.html',
+  men: './men.html',
+  women: './women.html',
+  product: './product.html',
+  cart: './cart.html',
+  wishlist: './wishlist.html',
+  checkout: './checkout.html',
+  contact: './contact.html',
+  blog: './blog.html',
+  'blog-detail': './blog-detail.html',
+  orders: './orders.html',  // 👈 ye add karo
+},
